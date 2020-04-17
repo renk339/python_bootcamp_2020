0a7e2de8961a2b8743d19662a1bd602d4d1775e7
Kacper Ren <renkacper1@gmail.com>
2020-04-18 01:46:23 +0200

    origin

---
+++ dzialania matematyczne.py
@@ -0,0 +1,22 @@
+print(2 - 3)
+print(2 * 4)
+print(9 / 3)
+print(9 % 5)
+print(2 ** 3)
+print(4 ** 0.5)
+print (1 + 3j)
+print()
+print(int())
+print()
+# liczby zmiennoprzecinkowe - float
+'''
+>, <, >=, <=, ==, !=
+'''
+print(1 > 3)
+print(1 <= 3)
+print(id(1))
+print(type(1))
+a = 10
+h = 5
+triangle_area = a * h / 2
+print("Pole trójkąta", triangle_area)
